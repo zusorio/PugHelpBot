ab5beadabce9e0be495ba87366f810decbda9f6d
ZusorCode <tobias.d.messner@gmail.com>
2019-05-30 12:59:18 -0600

    Reworked entire code into Cogs

---
+++ PugHelpBot/cogs/admin_tools.py
@@ -0,0 +1,27 @@
+from ..helpers import Config, is_mod
+import logging
+import discord
+from discord.ext import commands
+
+
+class AdminTools(commands.Cog):
+    """Cog for admin commands like change_min_reacts"""
+    def __init__(self, bot: commands.Bot, log: logging.Logger, config: Config):
+        self.bot = bot
+        self.log = log
+        self.config = config
+
+        self.log.info("Loaded Cog AdminTools")
+
+    @commands.command()
+    @commands.check(is_mod)
+    async def change_min_reacts(self, ctx: discord.ext.commands.context.Context, min_reacts: int):
+        self.config.set_min_players(min_reacts)
+        await ctx.author.send(f"Set the minimum amount of reacts to {min_reacts}")
+
+    @change_min_reacts.error
+    async def change_min_reacts_error(self, ctx: discord.ext.commands.context.Context, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.author.send("Sorry, something went wrong... You don't have permission to do that!")
+        else:
+            await ctx.author.send(error)
---
+++ PugHelpBot/cogs/simple_ping.py
@@ -0,0 +1,85 @@
+from ..helpers import send_ping, Config, get_unique_message_react_users, PingStatus
+import logging
+import discord
+from discord.ext import commands
+
+
+class SimplePing(commands.Cog):
+    """Cog for doing the regular ping reminders and checked pings"""
+    def __init__(self, bot: commands.Bot, log: logging.Logger, config: Config, ping_status: PingStatus):
+        self.bot = bot
+        self.log = log
+        self.config = config
+        self.ping_status = ping_status
+
+        self.log.info("Loaded Cog SimplePing")
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, _):
+        # If the reaction is on a message in the specified channels and wasn't pinged and notified for
+        if reaction.message.channel.name in self.config.allowed_channels:
+            if reaction.message.id not in self.ping_status.get_already_notified_simple():
+                if reaction.message.id not in self.ping_status.get_already_pinged_simple():
+                    # Get all unique users that reacted
+                    unique_reacts = await get_unique_message_react_users(reaction.message)
+                    # If there are the minimum of required reacts
+                    if len(unique_reacts) >= self.config.min_players:
+                        # Send the user instructions on how to mention all of the reactants
+                        # The command the user is told to use is (prefix)ping MESSAGE_ID_OF_ORIGINAL_POST
+                        await reaction.message.author.send(
+                            f"Your LFP Post reached the minimum of {self.config.min_players} reacts!\n"
+                            f"Reply with `{self.config.bot_prefix}ping {reaction.message.id}` "
+                            f"__**in this DM**__  to ping the people that want to join!")
+                        # Mark the message to not notify the user again
+                        self.ping_status.add_already_notified(reaction.message.id)
+
+    @commands.command()
+    async def ping(self, ctx: discord.ext.commands.context.Context, message: discord.Message):
+        """
+        A command using discord.py's bot framework. Used to mention the people that reacted to the message.
+        :param ctx: Discord context
+        :param message: The message ID of the message to ping for. The user get's this sent in the command they are told to use.
+        :return: Nothing
+        """
+
+        # Make sure that the message is in a correct channel and that we haven't pinged for it yet
+        if message.channel.name in self.config.allowed_channels and message.id not in self.ping_status.get_already_pinged_simple():
+            # Get all the users we need the mention for that message
+            users_to_mention = await get_unique_message_react_users(message)
+            # Make sure there are actually enough users for a ping
+            if len(users_to_mention) >= self.config.min_players:
+                await send_ping(message, users_to_mention)
+                # Add the message to the already_pinged list so that the user doesn't ping for the same message twice
+                self.ping_status.add_already_pinged(message.id)
+                # Tell the user that everything is done.
+                await ctx.author.send(
+                    "I mentioned all the people who reacted to your post in that channel. Make sure to put a message "
+                    "there telling them which lobby to join!")
+            else:
+                # Warn the user that there are not enough reactions.
+                await ctx.author.send(
+                    "The message doesn't have enough reacts yet! Don't try and write commands manually. I'll tell you "
+                    "once you can ping.")
+        else:
+            # If that's wrong send the user a warning.
+            await ctx.author.send(
+                "Somehow your command is messed up. Make sure you didn't already ping and that the number is correct!")
+        # Delete the message if it is not sent
+        if not isinstance(ctx.message.channel, discord.DMChannel):
+            await ctx.message.delete()
+
+    @ping.error
+    async def ping_error(self, ctx: discord.ext.commands.context.Context, error):
+        """
+        Error handler for the ping command
+        :param ctx: Context
+        :param error: Error message
+        :return: nothing
+        """
+        # If the message can't be found, send the user a warning
+        if isinstance(error, commands.BadArgument):
+            await ctx.author.send("Couldn't find that message. Make sure to copy and paste the command I sent you!")
+        else:
+            # For other errors, send a generic message and log the error
+            await ctx.author.send("Something went wrong... Sorry about that.")
+            self.log.info(error)
