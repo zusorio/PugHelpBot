719bef5dd44ebefb4d15f1a81679b95a3b57ceeb
Tobias Messner <tobias.d.messner@gmail.com>
2019-05-30 13:22:14 -0600

    Worked on Dockerfile to support new structure, fixed cogs perm checks not working

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ RUN pip3 install discord.py
 RUN mkdir -p /usr/src/bot
 WORKDIR /usr/src/bot
 
-COPY main.py /usr/src/bot
+COPY PugHelpBot/* /usr/src/bot
 COPY config.json /usr/src/bot
 
-ENTRYPOINT ["python3", "/usr/src/bot/main.py"]
+ENTRYPOINT ["python3", "-m", "PugHelpBot"]
--- PugHelpBot/cogs/advanced_tools.py
+++ PugHelpBot/cogs/advanced_tools.py
@@ -16,19 +16,21 @@ class AdvancedTools(commands.Cog):
         self.log.info("Loaded Cog AdvancedTools")
 
     @commands.command()
-    @commands.check(is_advanced)
     async def mod_ping(self, ctx: discord.ext.commands.context.Context, message: discord.Message):
-        # Bypasses all checks and just pings for any message. Just give it any message ID
-        users_to_mention = await get_unique_message_react_users(message)
-        await send_ping(message, users_to_mention)
-        # Tell the user that everything is done.
-        await ctx.author.send(
-            "I mentioned all the people who reacted to your post in that channel. Make sure to put a message "
-            "there telling them which lobby to join!")
-        # Add the message to the already_pinged and already_notified list
-        # Note: this does not prevent repeat staff pings
-        self.ping_status.add_already_notified(message.id)
-        self.ping_status.add_already_pinged(message.id)
+        if is_advanced(ctx, self.config):
+            # Bypasses all checks and just pings for any message. Just give it any message ID
+            users_to_mention = await get_unique_message_react_users(message)
+            await send_ping(message, users_to_mention)
+            # Tell the user that everything is done.
+            await ctx.author.send(
+                "I mentioned all the people who reacted to your post in that channel. Make sure to put a message "
+                "there telling them which lobby to join!")
+            # Add the message to the already_pinged and already_notified list
+            # Note: this does not prevent repeat staff pings
+            self.ping_status.add_already_notified(message.id)
+            self.ping_status.add_already_pinged(message.id)
+        else:
+            raise commands.CheckFailure
 
     @mod_ping.error
     async def change_min_reacts_error(self, ctx: discord.ext.commands.context.Context, error):
